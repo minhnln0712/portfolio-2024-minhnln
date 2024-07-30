1e259ff71932f0824f81a09f1477f696a3429bf1
minhnln0712 <minhnln0712@gmail.com>
2024-07-31 01:58:10 +0700

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 import * as THREE from "three";
 import Stats from "three/addons/libs/stats.module.js";
+// import {FPXLoader} from ""
 
 // Initialization
 const stats = new Stats();
@@ -21,9 +22,11 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(-5, 10, 0); // Camera Default Position
 // #endregion
 
-// #regin Renderer Init
+// #region Renderer Init
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 window.addEventListener("resize", () => {
@@ -36,33 +39,40 @@ window.addEventListener("resize", () => {
 function render() {
   renderer.render(scene, camera);
 }
+
 // #endregion
 
 // #region Light Init
 
-const ambientLight = new THREE.AmbientLight(0xeab676, 0.01);
+const ambientLight = new THREE.AmbientLight(0xeab676, 0.1);
 ambientLight.visible = true;
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
 directionalLight.visible = true;
-directionalLight.position.x = 10;
-directionalLight.castShadow = true;
+directionalLight.position.x = 5;
+directionalLight.position.z = 5;
+// directionalLight.shadow.camera.near = 0;
+// directionalLight.shadow.camera.far = 100;
+// directionalLight.castShadow = true;
 scene.add(directionalLight);
 
 // #endregion
 
+// #region Character Init
+
 const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(),
+  new THREE.ConeGeometry(),
   new THREE.MeshStandardMaterial({ color: 0xff0001 })
 );
 cube.position.y = 1;
 cube.castShadow = true;
 scene.add(cube);
-camera.lookAt(cube.position);
+camera.lookAt(cube.position); // Make camera look at the character Ref at the first time it renders!
 
-function BeginPlay() {}
-BeginPlay();
+// #endregion
+
+// #region Main Surface
 
 // Add a Plane for the surface
 const planeGeometry = new THREE.PlaneGeometry(1000, 1000, 1, 1);
@@ -70,13 +80,16 @@ const texture = new THREE.TextureLoader().load("/public/images/testText.jpg");
 const plane = new THREE.Mesh(planeGeometry, new THREE.MeshStandardMaterial());
 plane.rotateX(-Math.PI / 2);
 plane.receiveShadow = true;
+plane.castShadow = true;
 plane.material.map = texture;
 scene.add(plane);
 
-const clock = new THREE.Clock();
-let deltaTime: number;
+// #endregion
 
-// InputKey
+function BeginPlay() {}
+BeginPlay();
+
+// #region InputKey
 
 const keyMap: { [key: string]: boolean } = {};
 const onDocumentKey = (e: KeyboardEvent) => {
@@ -85,13 +98,34 @@ const onDocumentKey = (e: KeyboardEvent) => {
 document.addEventListener("keydown", onDocumentKey, false);
 document.addEventListener("keyup", onDocumentKey, false);
 
+const mouse = new THREE.Vector2();
+renderer.domElement.addEventListener("mousemove", (e) => {
+  mouse.set(
+    (e.clientX / renderer.domElement.clientWidth) * 2 - 1,
+    -(e.clientY / renderer.domElement.clientHeight) * 2 + 1
+  );
+
+  cube.lookAt(new THREE.Vector3(mouse.y, 0, mouse.x));
+  //   console.log("x: " + mouse.x);
+  //   console.log("y: " + mouse.y);
+  //   console.log(Math.cos(mouse.x));
+
+  //   cube.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), Math.cos(mouse.x) * 0.01);
+});
+
+// #endregion
+
+const clock = new THREE.Clock();
+let deltaTime: number;
+
 function EventTick() {
   requestAnimationFrame(EventTick);
   deltaTime = clock.getDelta();
 
   if (keyMap["KeyW"] || keyMap["ArrowUp"]) {
-    console.log("MoveUp");
+    console.log(cube.rotation.x);
     CharacterMoveUp(20);
+    // cube.rotation.x = 0.5 * Math.PI;
   }
   if (keyMap["KeyS"] || keyMap["ArrowDown"]) {
     CharacterMoveUp(-20);
@@ -102,7 +136,7 @@ function EventTick() {
   if (keyMap["KeyD"] || keyMap["ArrowRight"]) {
     CharacterMoveRight(20);
   }
-
+  //   cube.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), deltaTime * 0.5 * Math.PI);
   stats.update();
   render();
 }
