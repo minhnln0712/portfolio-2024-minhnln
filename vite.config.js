29c109280e7cfc07e96108f3dddcc02323343cf1
Minh Nguyen <minhnln0712@gmail.com>
2024-08-02 14:06:11 +0700

    release

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue';
+
 
 export default defineConfig({
     esbuild: {
         supported: {
             'top-level-await': true //browsers can handle top-level-await features
         },
-    }
+    },
+    plugins: [vue()],
+    server: {
+        // Ensure correct MIME type
+        mimeTypes: {
+            'application/javascript': ['js', 'ts'],
+        },
+    },
+
 })
