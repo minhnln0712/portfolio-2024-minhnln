f1e81d69b80f95ca2849f37339f753f72abbc960
Minh Nguyen <minhnln0712@gmail.com>
2024-08-02 14:15:00 +0700

    release

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue';
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 
 export default defineConfig({
@@ -8,12 +9,15 @@ export default defineConfig({
             'top-level-await': true //browsers can handle top-level-await features
         },
     },
-    plugins: [vue()],
+    plugins: [vue(), topLevelAwait()],
     server: {
         // Ensure correct MIME type
         mimeTypes: {
             'application/javascript': ['js', 'ts'],
         },
+        proxy: {
+            '/api': 'http://localhost:5000'
+        }
     },
-
+    base: './',
 })
